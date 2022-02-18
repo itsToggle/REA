a7ec190b518ec54b023a88a32f3cead164db448a
itsToggle <71379623+itsToggle@users.noreply.github.com>
2022-02-18 10:59:57 +0100

    Update netzwerke.py

--- netzwerke.py
+++ netzwerke.py
@@ -2,15 +2,30 @@ from sympy import * #Poly, degree, fraction, symbols, simplify, factor, mathemat
 from sympy.parsing.sympy_parser import parse_expr
 import copy
 
+class ZAdd(Add):
+    #Parallele "Addition" Definieren
+    def __or__(self,other):
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
+    def __ror__(self,other):
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
+class ZPow(Pow):
+    #Parallele "Addition" Definieren
+    def __or__(self,other):
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
+    def __ror__(self,other):
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
+class ZMul(Mul):
+    #Parallele "Addition" Definieren
+    def __or__(self,other):
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
+    def __ror__(self,other):
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
 class Z(Symbol):
     #Parallele "Addition" Definieren
     def __or__(self,other):
-        return 1/(1/self+1/other)
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
     def __ror__(self,other):
-        return 1/(1/self+1/other)
-
-Mul.__or__ = Z.__or__ 
-Mul.__ror__ = Z.__ror__ 
+        return ZPow(ZAdd(ZPow(self,Integer(-1)),ZPow(other,Integer(-1))),Integer(-1),evaluate=False)
 
 class R:
     def __new__(cls,name):
@@ -20,26 +35,23 @@ class R:
 class L:
     def __new__(cls,name):
         globals()[f"L{name}"] = Z("L"+name)
-        globals()[f"L{name}"] = s*globals()[f"L{name}"]
+        globals()[f"L{name}"] = ZMul(globals()[f"L{name}"],s)
         return globals()[f"L{name}"]
 class C:
     def __new__(cls,name):
         globals()[f"C{name}"] = Z("C"+name)
-        globals()[f"C{name}"] = 1/(s*globals()[f"C{name}"])
+        globals()[f"C{name}"] = ZPow(ZMul(globals()[f"C{name}"],s),Integer(-1),evaluate=False)
         return globals()[f"C{name}"]
 
 #Programm
 s = Z('s') 
-j = Z('j')
-w = Z('w')
-s = j*w
 
 #Bauteile instanziieren
-Lh = L('h') # Lx -> sLx
-Ch = C('h') # Cx -> 1/(sCx)
-Rs = R('s') # R  -> R
+ZLh = L('h') # ZLx -> sLx
+ZCh = C('h') # ZCx -> 1/(sCx)
+ZRs = R('s') # ZR  -> R
 
-Zteil = Lh|Ch
-Zges = Rs+(Lh|Ch)
+Zteil = (ZLh|ZCh)
+Zges = ZRs+(ZLh|ZCh)
 
 print("Zteil/Zges = " + str(mathematica_code(simplify(factor(Zteil/Zges)))))
